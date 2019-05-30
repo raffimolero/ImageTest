7ee31b6ce43a157ebf27bcde1cecaf80377aa175
Raffi Molero <raffimolero@gmail.com>
2019-05-30 15:29:01 +0800

    double buffering

--- ImageTest/ImageTest.h
+++ ImageTest/ImageTest.h
@@ -1,9 +1,12 @@
 #pragma once
 #include "Resource.h"
 #include "Sprite.h"
+#include "ImageOps.h"
 
+RECT window;
 int x, y, w, h;
 int id = 0;
+bool update = false;
 DWORD globalTime = 0;
 HBITMAP sheet;
 Sprite barrel;
@@ -13,44 +16,45 @@ inline HBITMAP LoadBmp(HINSTANCE hInst, int ID) {
 	return (HBITMAP)LoadImage(hInst, MAKEINTRESOURCE(ID), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
 }
 inline void updateWindowDimensions(HWND hWnd) {
-	RECT window;
 	GetWindowRect(hWnd, &window);
 	x = window.left;
 	y = window.top;
 	w = window.right - x;
 	h = window.bottom - y;
 }
-inline void createTimer(HWND hWnd, int msPerTick, TIMERPROC timerFunc) {
-	SetTimer(hWnd, id++, msPerTick, timerFunc);
-}
 
-void start() {
-	
-}
+void start() {}
 void paint(HDC &hdc) {
+	HDC buf = CreateCompatibleDC(hdc);
 	HDC mem = CreateCompatibleDC(hdc);
+	HBITMAP bufBmp = CreateCompatibleBitmap(hdc, w, h);
+	SelectObject(buf, bufBmp);
+
+	fillSolidRect(buf, 0, 0, w, h, hue(globalTime));
+
+	barrel.paint(buf, mem);
+	boom.paint(buf, mem);
+
+	DeleteDC(mem);
+	BitBlt(hdc, 0, 0, w, h, buf, 0, 0, SRCCOPY);
+	DeleteObject(bufBmp);
+	DeleteDC(buf);
+}
+void CALLBACK tick(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime) {
+	if (globalTime % 4 == 0) {
+		barrel.nextFrame();
+		boom.nextFrame();
+	}
 
 	double angle = tau / 256;
 	rotate(boom.x, boom.y, w * 2 / 5, h * 2 / 5, -angle);
 	rotate(barrel.x, barrel.y, boom.x, boom.y, angle);
-
-	barrel.paint(hdc, mem);
-	boom.paint(hdc, mem);
-
-	DeleteDC(mem);
-}
-void CALLBACK paintTick(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime) {
-	InvalidateRect(hwnd, NULL, true);
-}
-void CALLBACK tick(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime) {
-	barrel.nextFrame();
-	boom.nextFrame();
-
+	
+	InvalidateRect(hwnd, NULL, false);
 	globalTime++;
 }
 void init(HWND hWnd, HINSTANCE hInst) {
-	createTimer(hWnd, 10, paintTick);
-	createTimer(hWnd, 1000 / 24, tick);
+	SetTimer(hWnd, NULL, 15, tick);
 	updateWindowDimensions(hWnd);
 
 	sheet = LoadBmp(hInst, IDB_EXPLOSION);
